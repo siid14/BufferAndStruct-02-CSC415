09ce1240cd804da81f25f5e64a75b4c8502a8651
Sidney <sid.14boys@yahoo.fr>
2023-08-31 00:56:09 -0700

    minor add

---
+++ Thomas_Sidney_HW2_main.c
@@ -0,0 +1,12 @@
+/**************************************************************
+ * Class:  CSC-415-0# Summer 2021
+ * Name: Sidney Thomas
+ * Student ID: 918656419
+ * GitHub Name: bierman
+ * Project: Assignment 2 – Command Line Arguments
+ *
+ * File: Thomas_Sidney_HW2_main.c
+ *
+ * Description: To show how to use ...
+ *
+ **************************************************************/
