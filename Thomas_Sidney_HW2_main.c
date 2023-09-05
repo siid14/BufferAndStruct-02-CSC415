001efd852c9b62f342b578bca0f41df79ae720be
Sidney <sid.14boys@yahoo.fr>
2023-09-04 19:05:15 -0700

    step 6 ongoing

--- Thomas_Sidney_HW2_main.c
+++ Thomas_Sidney_HW2_main.c
@@ -14,6 +14,7 @@
 #include <stdio.h>  // library for input/output
 #include <stdlib.h> // library for memory management
 #include <string.h>
+#include <errno.h>
 #include "assignment2.h"
 
 int main(int argc, char *argumentValues[])
@@ -57,15 +58,63 @@ int main(int argc, char *argumentValues[])
     // check if the writePersonalInfo function was successful
     if (writeSuccess == 0)
     {
-        printf("Personal information written successfullu\n");
+        printf("Personal information written successfully\n");
     }
     else
     {
         printf("Failed to write personal information.\n");
     }
 
+    // create a buffer of size BLOCK_SIZE using malloc.
+    char *buffer = (char *)malloc(BLOCK_SIZE * sizeof(char));
+
+    // check if malloc was successful in allocating memory.
+    if (buffer == NULL)
+    {
+        perror("Failed to allocate memory for the buffer");
+        exit(1); // exit the program with an error code.
+    }
+
+    // initialize variables to keep track of the buffer position and data written.
+    size_t bufferPosition = 0; // current position in the buffer.
+    size_t dataWritten = 0;    // amount of data written to the buffer.
+
+    char *nextString;
+    while ((nextString = getNext()) != NULL)
+    {
+        // Process each received string in this loop.
+        size_t stringLength = strlen(nextString);
+        size_t spaceLeftInBuffer = BLOCK_SIZE - bufferPosition;
+
+        // Check if the string can fit in the remaining space in the buffer.
+        if (stringLength <= spaceLeftInBuffer)
+        {
+            // The entire string fits in the remaining space, so copy it to the buffer.
+            memcpy(buffer + bufferPosition, nextString, stringLength);
+            bufferPosition += stringLength;
+        }
+        else
+        {
+            // The string is larger than the remaining space in the buffer.
+            // Copy as much as possible into the buffer and commit the buffer.
+            size_t bytesToCopy = spaceLeftInBuffer;
+            memcpy(buffer + bufferPosition, nextString, bytesToCopy);
+            bufferPosition += bytesToCopy;
+
+            // Commit the filled buffer using commitBlock.
+            commitBlock(buffer);
+
+            // Reset the buffer position and copy the remaining part of the string.
+            bufferPosition = 0;
+            bytesToCopy = stringLength - spaceLeftInBuffer;
+            memcpy(buffer + bufferPosition, nextString + spaceLeftInBuffer, bytesToCopy);
+            bufferPosition += bytesToCopy;
+        }
+    }
+
     // done with the allocated memory, so freeing it
     free(studentInfo);
+    free(buffer);
 
     return 0; // exit successfully
 }
